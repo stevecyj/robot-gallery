07a6d741d19604664d1d707325f1873f8e50320f
stevecyj <ycsteven.tsao@gmail.com>
2022-03-02 16:42:20 +0800

    [mod]:tsconfig jsx mode

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,20 @@ import './App.css';
 import robots from './mockdata/robots.json';
 import Robot from './components/Robot';
 
+// const html = "<img onerror='alert(\"Hacked\")' src='invalid-image' />";
+// const jsHacked = "javascript: alert('Hacked!');";
+
 function App() {
   return (
-    <ul>
-      {robots.map((r) => (
-        <Robot id={r.id} email={r.email} name={r.name} />
-      ))}
-    </ul>
+    <>
+      {/* <div>{html}</div>
+      <a href={jsHacked}>my site</a> */}
+      <ul>
+        {robots.map((r) => (
+          <Robot id={r.id} email={r.email} name={r.name} />
+        ))}
+      </ul>
+    </>
   );
 }
 
